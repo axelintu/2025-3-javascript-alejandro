4edad9627a34690eb30defed3c833a340d61375a
axelintu <darklintumon@gmail.com>
2025-08-13 05:23:57 -0600

    Se agregó ejercicio 1.

---
+++ 02.Clases/semana1/ejercicio1.js
@@ -0,0 +1,10 @@
+const { ask } = require('../helpers/input.js');
+
+async function main() {
+    const name = await ask('¿Cómo te llamas? ');
+    const age = Number(await ask('¿Cuántos años tienes? '));
+    const food = await ask('¿Cuál es tu comida favorita? ');
+    console.log('Hola ' + name + '! Tienes: ' + age + ' años, y tu comida favorita es: ' + food);
+}
+
+main();
