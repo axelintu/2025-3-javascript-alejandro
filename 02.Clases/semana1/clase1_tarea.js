65103b96c00c450a8921bf4a1d288c563a0a8751
axelintu <darklintumon@gmail.com>
2025-08-13 05:50:13 -0600

    Se resolvió la tarea de la clase 1.

--- 02.Clases/semana1/clase1_tarea.js
+++ 02.Clases/semana1/clase1_tarea.js
@@ -6,6 +6,33 @@ async function main() {
   // 2.- Pedir el segundo número
   // 3.- Pedir operador
 
+  const numero1 = Number(await ask('Dame el primer número: '));
+  const numero2 = Number(await ask('Dame el segundo número: '));
+  const operador = await ask(`Introduce el operador de tu operación: + (suma), - (resta), * (multiplicación), / (división) `);
+  let resultado = 0;
+
+  switch(operador) {
+    default:
+      console.log(operador + ' no es un operador valido');
+      break;
+    case '+':
+      console.log('Suma');
+      resultado = numero1 + numero2;
+      break;
+    case '-':
+      console.log('Resta');
+      resultado = numero1 - numero2;
+      break;    
+    case '*':
+      console.log('Multiplicación');
+      resultado = numero1 * numero2;
+      break;
+    case '/':
+      console.log('División');
+      resultado = numero1 / numero2;
+      break;
+  }
+  console.log('El resultado de tu operación es: ' + resultado);
 }
 
 main();
