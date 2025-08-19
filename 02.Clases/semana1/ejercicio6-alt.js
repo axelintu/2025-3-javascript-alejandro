92161f1799c9241fa8e582100987c42bb674314b
axelintu <darklintumon@gmail.com>
2025-08-19 00:21:24 -0600

    Update ejercicio6-alt.js

--- 02.Clases/semana1/ejercicio6-alt.js
+++ 02.Clases/semana1/ejercicio6-alt.js
@@ -1,17 +1,8 @@
 const { ask } = require('../helpers/input.js');
 
 async function main() {
-  const diasSemana = [
-    'Lunes',
-    'Martes',
-    'Miércoles',
-    'Jueves',
-    'Viernes',
-    'Sábado',
-    'Domingo'
-  ];
-  let dia = new Number;
-  dia = await ask('Te voy a mostrar un día de la semana \nIngresa un número del 1 al 7: ');
+  const diasSemana = ['Lunes','Martes','Miércoles','Jueves','Viernes','Sábado','Domingo'];
+  const dia = await ask('Te voy a mostrar un día de la semana \nIngresa un número del 1 al 7: ');
   if(isNaN(dia) || !(dia >= 1 && dia <= 7)) {
     console.log(`${dia} ☠️ No es un número del 1 al 7`);
     return; 
