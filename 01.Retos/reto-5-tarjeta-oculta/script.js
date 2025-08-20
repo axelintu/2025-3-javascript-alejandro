abbc42f4f92988a4f22f02f4a44b753988412657
axelintu <darklintumon@gmail.com>
2025-08-19 22:46:58 -0600

    Deleted duplicated challenges.

--- 01.Retos/reto-5-tarjeta-oculta/script.js
+++
@@ -1,8 +0,0 @@
-// Instrucciones: completa el código para alternar la clase 'oculto' al hacer clic
-
-const button = document.getElementById('toggleButton');
-const card = document.getElementById('profileCard');
-
-button.addEventListener('click', () => {
-  // Tu código aquí
-});
